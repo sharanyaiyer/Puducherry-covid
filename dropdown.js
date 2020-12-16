6fa16bf45d39cd5b6050030da6cd1e0be25d05e2
sharanyaiyer <64903143+sharanyaiyer@users.noreply.github.com>
2020-12-16 18:27:10 +0530

    Delete dropdown.js

--- dropdown.js
+++
@@ -1,8 +0,0 @@
-function TestsFunction() {
-    var T = document.getElementById("TestsDiv"),
-        displayValue = "";
-    if (T.style.display == "")
-        displayValue = "none";
-
-    T.style.display = displayValue;
-}
